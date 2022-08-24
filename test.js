77554bc6fe17b4d8ebb153fa464b2f9d26991223
Katarzyna Fronc <katarzynaanna.fronc@gmail.com>
2022-08-24 10:46:43 +0200

    else if was added to test.js

--- test.js
+++ test.js
@@ -1,12 +1,19 @@
-var youLikeMeet = true;
-var myNum = 10;
+var myAge = 43;
 
-if (youLikeMeet){
+if (myAge > 30){
 
-    document.write("Here is the meaty menu.");
+    document.write("you are over 35!");
+
+} else if (myAge > 20){
+
+    document.write("you are over 20!");
+
+} else if (myAge > 10){
+
+    document.write("you are over 10!");
 
 } else{
-    
-    document.write("Here is the vegan menu")
+
+    document.write("you are not over 10!");
 
 }
