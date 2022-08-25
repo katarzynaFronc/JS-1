588086c04ff23cc3961fb062624c057b18d039be
Katarzyna Fronc <katarzynaanna.fronc@gmail.com>
2022-08-25 07:46:25 +0200

    test.js was modified, While Loop was added

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
-var myAge = 26;
+var age = 5;
 
-if (myAge < 18 || myAge > 30 || myAge === 25){
+while (age < 10) {
 
-    document.write("you ain't coming")
-    
-} else{
-
-    document.write("you can come");
+    console.log("your age is less than 10");
+    age ++;
 
 }
+
+document.write("you are now over 10");
